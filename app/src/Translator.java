9f1c6a389c92e477a4ab90eda4499dc9cc6ee63e
Carlos Esteban Guerrero Robles <carlos.guerrero@utec.edu.pe>
2019-06-17 10:52:53 -0500

    Fix translator to work on multi language

--- app/src/Translator.java
+++ app/src/Translator.java
@@ -1,3 +1,4 @@
+import java.util.Collection;
 import java.util.Dictionary;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
@@ -19,20 +20,129 @@ public class Translator {
         this.inputLanguage = inputLanguage;
         this.outputLanguage = outputLanguage;
 
-        dictionary.put("perro","dog");
-        dictionary.put("puerta","door");
-        dictionary.put("pizarra","board");
-        dictionary.put("casa","house");
-        dictionary.put("ventana","window");
-        dictionary.put("la","the");
-        dictionary.put("en","in");
-        dictionary.put("esta","is");
 
-        dictionary.put("chien","perro");
-        dictionary.put("chat","gato");
-        dictionary.put("mison","casa");
-        dictionary.put("ecran","pantalla");
-        dictionary.put("et","esta en");
+
+        // Español Ingles
+        if (inputLanguage == "es" && outputLanguage == "en") {
+            dictionary.put("perro","dog");
+            dictionary.put("puerta","door");
+            dictionary.put("pizarra","board");
+            dictionary.put("casa","house");
+            dictionary.put("ventana","window");
+            dictionary.put("la","the");
+            dictionary.put("en","in");
+            dictionary.put("esta","is");
+        }
+        if (inputLanguage == "en" && outputLanguage == "es") {
+            dictionary.put("dog", "perro");
+            dictionary.put("door", "puerta");
+            dictionary.put("board", "pizarra");
+            dictionary.put("casa","house");
+            dictionary.put("window", "ventana");
+            dictionary.put("the", "la");
+            dictionary.put("in", "en");
+            dictionary.put("is", "esta");
+        }
+
+        // Español Frances
+        if (inputLanguage == "fr" && outputLanguage == "es") {
+            dictionary.put("chien","perro");
+            dictionary.put("chat","gato");
+            dictionary.put("mison","casa");
+            dictionary.put("ecran","pantalla");
+            dictionary.put("et","esta en");
+        }
+        if (inputLanguage == "es" && outputLanguage == "fr") {
+            dictionary.put("perro", "chien");
+            dictionary.put("gato", "chat");
+            dictionary.put("casa", "mison");
+            dictionary.put("pantalla", "ecran");
+            dictionary.put("esta en", "et");
+        }
+
+        //Español Portugues
+        if (inputLanguage == "es" && outputLanguage == "pr") {
+            dictionary.put("perro", "cão");
+            dictionary.put("gato", "gato");
+            dictionary.put("casa", "lar");
+            dictionary.put("pantalla", "tela");
+            dictionary.put("esta", "esta");
+            dictionary.put("en", "em");
+        }
+        if (inputLanguage == "pr" && outputLanguage == "es") {
+            dictionary.put("cão", "perro");
+            dictionary.put("gato", "gato");
+            dictionary.put("lar", "casa");
+            dictionary.put("tela", "pantalla");
+            dictionary.put("esta", "esta");
+            dictionary.put("em", "en");
+        }
+
+        // Frances Ingles
+        if (inputLanguage == "fr" && outputLanguage == "en") {
+            dictionary.put("chien","dog");
+            dictionary.put("porte","door");
+            dictionary.put("planche","board");
+            dictionary.put("maison","house");
+            dictionary.put("fenêtre","window");
+            dictionary.put("la","the");
+            dictionary.put("dans","in");
+            dictionary.put("est","is");
+        }
+        if (inputLanguage == "en" && outputLanguage == "fr") {
+            dictionary.put("dog", "chien");
+            dictionary.put("door", "porte");
+            dictionary.put("board", "planche");
+            dictionary.put("house","maison");
+            dictionary.put("window", "la fenêtre");
+            dictionary.put("the", "la");
+            dictionary.put("in", "dans");
+            dictionary.put("is", "est");
+        }
+
+        //Frances Portugues
+        if (inputLanguage == "fr" && outputLanguage == "pr") {
+            dictionary.put("chien","cão");
+            dictionary.put("porte","porta");
+            dictionary.put("planche","borda");
+            dictionary.put("maison","casa");
+            dictionary.put("fenêtre","janela");
+            dictionary.put("la","a");
+            dictionary.put("dans","em");
+            dictionary.put("est","é");
+        }
+        if (inputLanguage == "pr" && outputLanguage == "fr") {
+            dictionary.put("cão", "chien");
+            dictionary.put("porta", "porte");
+            dictionary.put("borda", "planche");
+            dictionary.put("casa","maison");
+            dictionary.put("janela", "la fenêtre");
+            dictionary.put("aes", "la");
+            dictionary.put("em", "dans");
+            dictionary.put("é", "est");
+        }
+
+        //Ingles portugues
+        if (inputLanguage == "pr" && outputLanguage == "en") {
+            dictionary.put("cão","dog");
+            dictionary.put("porta","door");
+            dictionary.put("borda","board");
+            dictionary.put("casa","house");
+            dictionary.put("janela","window");
+            dictionary.put("aes","the");
+            dictionary.put("em","in");
+            dictionary.put("é","is");
+        }
+        if (inputLanguage == "en" && outputLanguage == "pr") {
+            dictionary.put("dog", "cão");
+            dictionary.put("door", "porta");
+            dictionary.put("board", "borda");
+            dictionary.put("house","casa");
+            dictionary.put("window", "janela");
+            dictionary.put("the", "aes");
+            dictionary.put("in", "em");
+            dictionary.put("is", "é");
+        }
 
 //        try {
 //            File inputFile = new File(wordFile);
@@ -76,7 +186,7 @@ public class Translator {
     }
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-        Translator translator = new Translator("resources/es-en.xml", "Spanish", "Enaglish");
+        Translator translator = new Translator("resources/es-en.xml", "es", "en");
         System.out.println(translator.getTranslate("Juan's Perro esta en la puerta"));
         System.out.println(translator.getTranslate("chien et ecran"));
         System.out.println(translator.getTranslate("puerta"));
